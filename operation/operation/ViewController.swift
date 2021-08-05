4ed18d40697c567e23ca313c87d104a8f2314c99
DDS-Hiep <hiep.hoang@ddsolution.vn>
2021-08-05 16:56:39 +0700

    import rx

--- operation/operation/ViewController.swift
+++ operation/operation/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
-
+    private let disposeBag = DisposeBag()
+    let hello = Observable.from(["0","1","2","3","4","5","6","7","8","9","10","11","12"])
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        demo()
     }
-
-
-}
-
+    
+    func demo(){
+        demoReduce()
+    }
+    
+    //MARK:- Reduce
+    func demoReduce(){
+        hello.reduce("", accumulator: +)
+            .subscribe(onNext:{ value in
+                print(value)
+            }, onError: {error in
+                print(error)
+            },onCompleted: {
+                print("completed")
+            }).disposed(by: disposeBag)
+        
+    }
+    
---
+++ operation/operation/ViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ViewModel.swift
+//  operation
+//
+//  Created by DD Solution on 05/08/2021.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class ViewModel{
+    
+}
